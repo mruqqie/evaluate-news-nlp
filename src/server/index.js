2e3148edafb42409dd2b85d5d1c8a2d1617265b5
Ruqouyyah Muhammad <99969515+mruqqie@users.noreply.github.com>
2022-12-10 03:43:59 -0800

    modified:   .gitignore 	modified:   package-lock.json 	modified:   package.json 	modified:   src/client/index.js 	renamed:    src/client/styles/base.css -> src/client/styles/base.scss 	renamed:    src/client/styles/footer.css -> src/client/styles/footer.scss 	renamed:    src/client/styles/form.css -> src/client/styles/form.scss 	renamed:    src/client/styles/header.css -> src/client/styles/header.scss 	renamed:    src/client/styles/resets.css -> src/client/styles/resets.scss 	modified:   src/server/index.js 	modified:   webpack.dev.js 	modified:   webpack.prod.js

--- src/server/index.js
+++ src/server/index.js
@@ -1,23 +1,38 @@
-var path = require('path')
-const express = require('express')
+const path = require('path')
+const express = require('express');
 const mockAPIResponse = require('./mockAPI.js')
+const dotenv = require('dotenv');
+dotenv.config();
 
-const app = express()
+const app = express();
+
+//middleware
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+//cors for cross origin allowance
+const cors = require('cors');
+app.use(cors());
 
 app.use(express.static('dist'))
 
-console.log(__dirname)
+console.log(__dirname);
 
 app.get('/', function (req, res) {
-    // res.sendFile('dist/index.html')
-    res.sendFile(path.resolve('src/client/views/index.html'))
+    res.sendFile('dist/index.html')
+    // res.sendFile(path.resolve('src/client/views/index.html'))
 })
 
 // designates what port the app will listen to for incoming requests
 app.listen(8080, function () {
     console.log('Example app listening on port 8080!')
-})
+});
 
 app.get('/test', function (req, res) {
     res.send(mockAPIResponse)
-})
+});
+
+const apiKey = process.env.API_KEY;
+const baseUrl = "https://api.meaningcloud.com/sentiment-2.1?key="
